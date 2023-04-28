e2c93fae2ec9703722a45c2bdbb0df439cdff6a2
kanha05 <kanhasongeam2309@gmail.com>
2023-04-28 12:46:41 +0700

    first

---
+++ my-project/storage/framework/views/5cf13c95b6b456e0d975a7be4d7c0601.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html><?php /**PATH C:\capstone-project\my-project\resources\views/app.blade.php ENDPATH**/ ?>
